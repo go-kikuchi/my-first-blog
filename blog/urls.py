10aa888a6fcd5e68c5390855cdfe6690789ab0c6
Your Name <you@example.com>
2023-03-02 15:36:39 +0900

    Changed the HTML for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
+urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
