846c8fecc641cd1484e5ac77fcf2c49e45e560d9
go-kikuchi <go_kikuchi@nuco.co.jp>
2023-03-28 15:29:50 +0900

    DEBUG=Falseに変更

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f4nm=)yqw+sgf!7=^%%we2r-5)&j)##__#7dpty(lw1o5_x$a5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
